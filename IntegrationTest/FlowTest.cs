7d4cbe42cad8b2b35ea225f94bd4f4a91dc72e18
Bajrang Harlalka <bajrang.harlalka@dell.com>
2020-07-03 11:28:27 +0000

    Signed-off-by: Bajrang Harlalka <bajrang.harlalka@dell.com>

--- IntegrationTest/FlowTest.cs
+++ IntegrationTest/FlowTest.cs
@@ -26,6 +26,8 @@ namespace IntegrationTest
                 .Database("tracker_registration_dotnet_test")
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDREGISTERWITHEUREKA", "false")
                 .SetEnvironmentVariable("DISABLE_AUTH", "true")
+                .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__ENABLED", "false")
+                .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__FAILFAST", "false")
                 .Build();
 
             _allocationsServer = TestAppServerBuilder()
@@ -35,6 +37,8 @@ namespace IntegrationTest
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .SetEnvironmentVariable("DISABLE_AUTH", "true")
+                .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__ENABLED", "false")
+                .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__FAILFAST", "false")
                 .Build();
 
             _backlogServer = TestAppServerBuilder()
@@ -43,7 +47,9 @@ namespace IntegrationTest
         .Database("tracker_backlog_dotnet_test")
         .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
         .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")        .SetEnvironmentVariable("DISABLE_AUTH", "true")
-        .Build();
+        .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__ENABLED", "false")
+                .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__FAILFAST", "false")
+                .Build();
 
     _timesheetsServer = TestAppServerBuilder()
         .AppName("TimesheetsServer")
@@ -52,7 +58,9 @@ namespace IntegrationTest
         .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
         .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
           .SetEnvironmentVariable("DISABLE_AUTH", "true")
-        .Build();
+        .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__ENABLED", "false")
+                .SetEnvironmentVariable("SPRING__CLOUD__CONFIG__FAILFAST", "false")
+                .Build();
     }
 
         [Fact]
